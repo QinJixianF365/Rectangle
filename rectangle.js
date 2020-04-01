fb037e90a7169e3eeb7a2a1dcc40fedf86a41c7c
秦继贤 <1653764529@qq.com>
2020-04-01 16:30:02 +0800

    rectangle01

---
+++ rectangle.js
@@ -0,0 +1,25 @@
+$(function() {
+  // get dom elem
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+
+  /*calc button click event */
+  $btnCal.click(function() {
+    // get value
+    var w = Number($width.val()),
+        h = Number($height.val());
+
+    // calculate
+    var p = 2 * (w + h),
+        a = w * h;
+
+    // output
+    $perimeter.val(p);
+    $area.val(a);
+    return Math.round(w * Math.pow(10, h)) / Math.pow(10, h);
+  });
+    // console.log("hello world!");
+});
